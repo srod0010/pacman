48668ff1d8d3d13c62284e29240320c072011fc8
Savio Rodrigues <saviorodrigues@Savios-MacBook-Pro.local>
2019-01-10 12:00:23 -0800

    Add DOMContentLoaded callback

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import _ from 'lodash';
+const MovingObject = require('./moving_object.js');
+// window.MovingObject = MovingObject;
 
-function component() {
-    let element = document.createElement('div');
+const mo = new MovingObject({
+    pos: [30, 30],
+    vel: [10, 10],
+    radius: 5,
+    color: "#00FF00"
+});
 
-   
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+document.addEventListener("DOMContentLoaded", event => {
+    const canvasEl = document.getElementsByTagName("canvas")[0];
+    let c = canvasEl.getContext("2d");
+    canvasEl.height = window.innerHeight;
+    canvasEl.width = window.innerWidth;
+    mo.draw(c);
+    mo.move();
+    mo.draw(c);
+    mo.move();
+    mo.draw(c);
+})
 
-    return element;
-}
 
-document.body.appendChild(component());
+
+
+
+console.log('webpack is working');
+ 
+
