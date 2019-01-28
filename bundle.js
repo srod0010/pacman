db0235b82bd88deea2e800273bb2659cc1e62923
Savio Rodrigues <saviorodrigues@Savios-MacBook-Pro.local>
2019-01-27 20:17:25 -0800

    Add capture detection

--- bundle.js
+++ bundle.js
@@ -107,18 +107,42 @@ class Game {
         this.map = map;
         this.walls = new Walls(ctx, this.map);
         this.pacman = new Pacman(ctx, this.map);
-        this.shadowGhost = new Ghost(ctx, this.map, 285,300,'purple');
-        this.speedyGhost = new Ghost(ctx, this.map, 285, 290, 'pink');
-        this.bashfulGhost = new Ghost(ctx, this.map, 280,310,'cyan');
-        this.pokeyGhost = new Ghost(ctx, this.map, 284,320,'orange');
+        // this.shadowGhost = new Ghost(ctx, this.map, 285,300,'purple');
+        // this.speedyGhost = new Ghost(ctx, this.map, 285, 290, 'pink');
+        // this.bashfulGhost = new Ghost(ctx, this.map, 280,310,'cyan');
+        // this.pokeyGhost = new Ghost(ctx, this.map, 284,320,'orange');
+        this.ghosts = [];
         this.winner = false;
+        this.loser = false;
         this.started = false;
+        this.createGhosts();
+
         
     }
 
+    createGhosts() {
+        this.ghosts[0] = new Ghost(this.ctx, this.map, 285, 300, 'purple');
+        this.ghosts[1] = new Ghost(this.ctx, this.map, 285, 290, 'pink');
+        this.ghosts[2] = new Ghost(this.ctx, this.map, 280, 310, 'cyan');
+        this.ghosts[3] = new Ghost(this.ctx, this.map, 284, 320, 'orange');
+    }
+
+    captured() {
+        let caught = false;
+         this.ghosts.forEach((ghost) => {
+             if (this.pacman.x <= ghost.x + 20 &&
+                 this.pacman.x >= ghost.x - 20 && this.pacman.y <= ghost.y + 20 &&
+                 this.pacman.y >= ghost.y - 20) {
+                 caught = true;
+             }
+         });
+         return caught;
+    }
+
     gameOver() {
-        if (this.pacman.pillCount === 0) {
+        if (this.pacman.pillCount === 0 || this.captured()) {
             // console.log('ate all the pills');
+            console.log('hellowalsfk')
             this.winner = true;
             return true;
         } else {
@@ -139,10 +163,11 @@ class Game {
                  console.log(this.started);
                  this.walls.render();
                  this.pacman.draw();
-                 this.shadowGhost.draw();
-                 this.speedyGhost.draw();
-                 this.pokeyGhost.draw();
-                 this.bashfulGhost.draw();
+                //  this.shadowGhost.draw();
+                //  this.speedyGhost.draw();
+                //  this.pokeyGhost.draw();
+                //  this.bashfulGhost.draw();
+                this.ghosts.forEach(ghost => ghost.draw());
              }
         }
       
